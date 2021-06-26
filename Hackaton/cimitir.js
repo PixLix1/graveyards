b8e289753a06ba71389a5304eb364e69504896e2
AdiCotirla <82831502+AdiCotirla@users.noreply.github.com>
2021-06-26 21:56:59 +0300

    Add files via upload

---
+++ Hackaton/cimitir.js
@@ -0,0 +1,13 @@
+const Disponibile = document.getElementById('disponibile')
+const parcela1 = document.getElementById('parcela1')
+const random = Math.floor(Math.random()* 10 )
+
+
+parcela1.addEventListener('click', function()
+{
+    Disponibile.innerText = 2412412412
+})
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     Disponibile.innerText = random
+//   }, false);
